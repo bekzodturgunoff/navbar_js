4515b6d522b48b1e51497cf36b9dea42da3649fb
Bekzod <bekzodturgunoff@gmail.com>
2022-12-13 13:09:49 +0500

    YouTube solution

--- app.js
+++ app.js
@@ -3,8 +3,27 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
-const navToggle = document.querySelector(".nav-toggle");
-let links = document.querySelector(".links");
+
+// My solution
+// const navToggle = document.querySelector(".nav-toggle");
+// let links = document.querySelector(".links");
+// function isToggleOn() {
+//   links.classList.toggle("show-links");
+// }
+
 function isToggleOn() {
-  links.classList.toggle("show-links");
+  console.log("Cliked");
 }
+
+// Youtuber's soliton
+const navToggle = document.querySelector(".nav-toggle");
+
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
+});
